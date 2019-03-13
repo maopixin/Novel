e6785ffdcd973aea9f6e787aae57e84c20651b12
maopixin <18364076997@163.com>
2019-03-13 15:01:53 +0800

    手动升级0.59版本，运行已通过

--- ios/tests/AppDelegate.h
+++ ios/tests/AppDelegate.h
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <UIKit/UIKit.h>
+#import <React/RCTBridgeDelegate.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
